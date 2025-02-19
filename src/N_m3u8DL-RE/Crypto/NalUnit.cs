7f9eaadfa20b89f8c330c1e8c73ca0df2cb7d361
titus <titus.oxx@gmail.com>
2025-02-19 16:27:24 +0100

    + Sample AES support

---
+++ src/N_m3u8DL-RE/Crypto/NalUnit.cs
@@ -0,0 +1,109 @@
+﻿using System.Security.Cryptography;
+using MediaFormatLibrary.H264.Enums;
+
+namespace N_m3u8DL_RE.Crypto;
+
+class NalUnit
+{
+    private static readonly byte[] SyncByteSeqStart = [0x00, 0x00, 0x00, 0x01];
+    private static readonly byte[] SyncByteSeq = [0x00, 0x00, 0x01];
+    
+    public required NalUnitType Type { get; init; }
+    public required byte[] Data { get; set; }
+    public required int Length { get; set; }
+    public required int StartCodeLength { get; init; }
+    
+    private static int GetStartCodeLength(ReadOnlySpan<byte> input)
+    {
+        return input.Length switch
+        {
+            > 4 when input[0] == SyncByteSeqStart[0] && 
+                     input[1] == SyncByteSeqStart[1] && 
+                     input[2] == SyncByteSeqStart[2] && 
+                     input[3] == SyncByteSeqStart[3] => 4,
+            > 3 when input[0] == SyncByteSeq[0] && 
+                     input[1] == SyncByteSeq[1] && 
+                     input[2] == SyncByteSeq[2] => 3,
+            _ => throw new InvalidOperationException($"Invalid Start Code, {Convert.ToHexString(input[..4])}")
+        };
+    }
+    
+    public static (NalUnit Unit, byte[] Data) GetNext(byte[] data)
+    {
+        var startCodeLength = GetStartCodeLength(data);
+        
+        var next = data.AsSpan(startCodeLength);
+
+        var index1 = next.IndexOf(SyncByteSeqStart.AsSpan());
+        var index2 = next.IndexOf(SyncByteSeq.AsSpan());
+        
+        var nextPos = index1 != -1 ? index1 + startCodeLength : index2 != -1 ? index2 + startCodeLength : data.Length;
+
+        next = data.AsSpan(nextPos);
+        var payload = data.AsSpan(startCodeLength, nextPos - startCodeLength);
+
+        return (new NalUnit
+        {
+            Type = (NalUnitType)(payload[0] & 0x1f),
+            Data = payload.ToArray(),
+            Length = nextPos - startCodeLength,
+            StartCodeLength = startCodeLength
+        }, next.ToArray());
+    }
+    
+    private void RemoveScep3Bytes()
+    {
+        int i = 0, j = 0;
+    
+        while (i < Length)
+        {
+            if (Length - i > 3 && Data[i] == 0 && Data[i + 1] == 0 && Data[i + 2] == 3)
+            {
+                Data[j++] = Data[i++];
+                Data[j++] = Data[i++];
+                i++;
+            }
+            else
+            {
+                Data[j++] = Data[i++];
+            }
+        }
+
+        var newData = new byte[j];
+        Array.Copy(Data, newData, j);
+        Data = newData;
+        Length = j;
+    }
+    
+    public void Decrypt(Aes aes)
+    {
+        if (Data.Length <= 48)
+            return;
+
+        RemoveScep3Bytes();
+
+        if (Data.Length <= 32)
+            return;
+
+        using var transform = aes.CreateDecryptor(aes.Key, aes.IV);
+        
+        /* Required when running in N_m3u8DL-RE for some reason */
+        transform.TransformBlock(new byte[16], 0, 16, new byte[16], 0);
+        
+        var remaining = Data.Length - 32;
+        while (remaining > 0)
+        {
+            if (remaining > 16)
+            {
+                remaining -= transform.TransformBlock(Data, Data.Length - remaining, 16, Data, Data.Length - remaining);
+            }
+            remaining -= Math.Min(144, remaining);
+        }
+    }
+
+    public byte[] Write()
+    {
+        var syncByte = StartCodeLength == 4 ? SyncByteSeqStart : SyncByteSeq;
+        return [..syncByte, ..Data];
+    }
+}
---
+++ src/N_m3u8DL-RE/Crypto/SampleAesUtil.cs
@@ -0,0 +1,195 @@
+﻿using System.Security.Cryptography;
+using MediaFormatLibrary.AAC.ADTS;
+using MediaFormatLibrary.H264.Enums;
+using MediaFormatLibrary.Mpeg2;
+using MediaFormatLibrary.Mpeg2.Enums;
+using MediaFormatLibrary.Mpeg2.Helpers;
+using MediaFormatLibrary.Mpeg2.PES;
+using MediaFormatLibrary.Mpeg2.PES.Enums;
+using MediaFormatLibrary.Mpeg2.PSI;
+
+namespace N_m3u8DL_RE.Crypto;
+
+public static class SampleAesUtil
+{
+    public static void DecryptSampleAes(string filePath, byte[] keyByte, byte[] ivByte)
+    {
+        using var aes = Aes.Create();
+
+        aes.Key = keyByte;
+        aes.IV = ivByte;
+        aes.Mode = CipherMode.CBC;
+        aes.Padding = PaddingMode.None;
+        
+        var readStream = Mpeg2TransportStream.OpenTs(filePath, FileMode.Open, FileAccess.Read);
+        var streamReader = new Mpeg2TransportStreamReader(readStream);
+
+        var memoryStream = new MemoryStream();
+        var writeStream = new Mpeg2TransportStream(memoryStream, false);
+        var streamWriter = new Mpeg2TransportStreamWriter(writeStream);
+
+        var pidMap = new Dictionary<ushort, StreamType>();
+        
+        while (streamReader.ReadPacketSequence() is { } tsPayload)
+        {
+            var packets = tsPayload.Packets!;
+            var first = packets.First();
+
+            switch (tsPayload.Type)
+            {
+                case TsPayloadType.Pmt:
+                    var psis = ProgramSpecificInformationSection.ReadSection(first.Payload!);
+                    
+                    if (psis is ProgramMapSection pmt)
+                    {
+                        foreach (var entry in pmt.StreamEntries)
+                        {
+                            pidMap[entry.Pid] = entry.StreamType;
+                                
+                            entry.StreamType = entry.StreamType switch
+                            {
+                                (StreamType)0xDB => StreamType.H264,
+                                (StreamType)0xCF => StreamType.Mpeg2AdtsAac,
+                                (StreamType)0xC1 => StreamType.DolbyDigital,
+                                (StreamType)0xC2 => StreamType.DolbyDigitalPlus,
+                                _ => entry.StreamType
+                            };
+                        }
+                    }
+                    
+                    writeStream.WritePacket(first);
+                    break;
+                case TsPayloadType.Pes:
+                    var payload = TransportPacketHelper.AssemblePayload(packets);
+                    var pesPacket = new PesPacket(payload);
+
+                    if (pesPacket.Data!.Length < 3)
+                    {
+                        streamWriter.WritePesPacket(first.Header.Pid, pesPacket);
+                        break;
+                    }
+                    
+                    var streamType = pidMap[first.Header.Pid];
+                    switch (streamType)
+                    {
+                        case (StreamType)0xDB:
+                            pesPacket.DecryptH264Video(aes);
+                            break;
+                        case (StreamType)0xCF:
+                            pesPacket.DecryptAacAudio(aes);
+                            break;
+                        case (StreamType)0xC1:
+                            pesPacket.DecryptAc3Audio(aes);
+                            break;
+                        case (StreamType)0xC2:
+                            pesPacket.DecryptEac3Audio(aes);
+                            break;
+                    }
+
+                    streamWriter.WritePesPacket(first.Header.Pid, pesPacket);
+                    break;
+                default:
+                    writeStream.WritePacket(first);
+                    break;
+            }
+        }
+
+        readStream.BaseStream.Dispose();
+
+        File.WriteAllBytes(filePath, [..memoryStream.GetBuffer().Take((int)memoryStream.Length)]);
+
+        writeStream.BaseStream.Dispose();
+    }
+
+    private static void DecryptH264Video(this PesPacket packet, Aes aes)
+    {
+        var bytes = new List<byte>();
+        
+        var data = packet.Data!;
+        while (data.Length != 0 && NalUnit.GetNext(data) is var next)
+        {
+            data = next.Data;
+            
+            if (next.Unit.Type is NalUnitType.CodedSliceNonIdr or NalUnitType.CodedSliceIdr)
+            {
+                next.Unit.Decrypt(aes);
+            }
+
+            bytes.AddRange(next.Unit.Write());
+        }
+
+        packet.Data = bytes.ToArray();
+    }
+
+    private static void DecryptAacAudio(this PesPacket packet, Aes aes)
+    {
+        using var transform = aes.CreateDecryptor(aes.Key, aes.IV);
+        
+        /* Required when running in N_m3u8DL-RE for some reason */
+        transform.TransformBlock(new byte[16], 0, 16, new byte[16], 0);
+        
+        using var stream = new MemoryStream(packet.Data!);
+        
+        var frame = new AdtsFrame(stream);
+        
+        foreach (var block in frame.RawDataBlocks)
+        {
+            var remaining = block!.Length - 16;
+            while (remaining >= 16)
+            {
+                remaining -= transform.TransformBlock(block, block.Length - remaining, 16, block, block.Length - remaining);
+            }
+        }
+        
+        stream.Seek(0, SeekOrigin.Begin);
+        frame.WriteBytes(stream);
+    }
+
+    private static void DecryptAc3Audio(this PesPacket packet, Aes aes)
+    {
+        using var transform = aes.CreateDecryptor(aes.Key, aes.IV);
+        
+        /* Required when running in N_m3u8DL-RE for some reason */
+        transform.TransformBlock(new byte[16], 0, 16, new byte[16], 0);
+        
+        var readStream = new MemoryStream(packet.Data!);
+        var writeStream = new MemoryStream(packet.Data!);
+                            
+        while (readStream.Position < readStream.Length && new MediaFormatLibrary.AC3.SyncFrame(readStream) is var frame)
+        {
+            var data = frame.GetBytes();
+            
+            var remaining = data.Length - 16;
+            while (remaining >= 16)
+            {
+                remaining -= transform.TransformBlock(data, data.Length - remaining, 16, data, data.Length - remaining);
+            }
+            
+            writeStream.Write(data, 0, data.Length);
+        }
+    }
+
+    private static void DecryptEac3Audio(this PesPacket packet, Aes aes)
+    {
+        using var transform = aes.CreateDecryptor(aes.Key, aes.IV);
+        
+        /* Required when running in N_m3u8DL-RE for some reason */
+        transform.TransformBlock(new byte[16], 0, 16, new byte[16], 0);
+        
+        var readStream = new MemoryStream(packet.Data!);
+        var writeStream = new MemoryStream(packet.Data!);
+                            
+        while (readStream.Position < readStream.Length && new MediaFormatLibrary.E_AC3.SyncFrame(readStream) is var frame)
+        {
+            var data = frame.GetBytes();
+            
+            var remaining = data.Length - 16;
+            while (remaining >= 16)
+            {
+                remaining -= transform.TransformBlock(data, data.Length - remaining, 16, data, data.Length - remaining);
+            }
+            
+            writeStream.Write(data, 0, data.Length);
+        }
+    }
+}
