a95ed378a947c2fe779655726d66c3dbb6040df9
nilaoda <nilaoda@live.com>
2025-07-01 23:02:57 +0800

    优化直播流判断

--- src/N_m3u8DL-RE.Parser/Extractor/HLSExtractor.cs
+++ src/N_m3u8DL-RE.Parser/Extractor/HLSExtractor.cs
@@ -254,6 +254,10 @@ internal class HLSExtractor : IExtractor
                 segment.StartRange = o ?? segments.Last().StartRange + segments.Last().ExpectLength;
                 expectSegment = true;
             }
+            else if (line.StartsWith(HLSTags.ext_x_playlist_type))
+            {
+                isEndlist = line.Trim().EndsWith("VOD");
+            }
             // 国家地理去广告
             else if (line.StartsWith("#UPLYNK-SEGMENT"))
             {
