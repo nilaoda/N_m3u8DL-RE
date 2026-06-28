df70f0b3da0c630bd413bf617e758051f6b64757
nilaoda <nilaoda@live.com>
2026-06-29 00:08:05 +0800

    0.6.0

--- src/N_m3u8DL-RE/CommandLine/CommandInvoker.cs
+++ src/N_m3u8DL-RE/CommandLine/CommandInvoker.cs
@@ -15,7 +15,7 @@ namespace N_m3u8DL_RE.CommandLine;
 
 internal static partial class CommandInvoker
 {
-    public const string VERSION_INFO = "N_m3u8DL-RE (Beta version) 20251228";
+    public const string VERSION_INFO = "N_m3u8DL-RE (Beta version) 20260628";
 
     [GeneratedRegex("((best|worst)\\d*|all)")]
     private static partial Regex ForStrRegex();
